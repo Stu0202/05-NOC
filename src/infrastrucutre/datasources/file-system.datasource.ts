9d2f904b5ea335f0529480b144ead97b4f4ae430
Jorge <jorgereyeslira02@gmail.com>
2024-09-17 21:55:05 -0500

    SaveLog

--- src/infrastrucutre/datasources/file-system.datasource.ts
+++ src/infrastrucutre/datasources/file-system.datasource.ts
@@ -1,3 +1,4 @@
+import { log } from "util";
 import { LogDataSource } from "../../domain/datasources/log.datasource";
 import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
 import fs from 'fs'
@@ -5,24 +6,57 @@ import fs from 'fs'
 
 
 
-export class FileSystemDatasource implements LogDataSource{
+export class FileSystemDatasource implements LogDataSource {
 
-    private readonly logPath  ='logs/'
-    private readonly allLogsPath  ='logs/logs-low.log'
-    private readonly mediumLogsPath  ='logs/logs-medium.log'
-    private readonly highLogsPath  ='logs/logs-high.log'
+    private readonly logPath = 'logs/'
+    private readonly allLogsPath = 'logs/logs-all.log'
+    private readonly mediumLogsPath = 'logs/logs-medium.log'
+    private readonly highLogsPath = 'logs/logs-high.log'
 
-    constructor(){
-
+    constructor() {
+        this.createLogsfiles()
     }
 
 
-    private createLogsfile =() => {
+    private createLogsfiles = () => {
+        if (!fs.existsSync(this.logPath)) {
+            fs.mkdirSync(this.logPath)
+        }
+
+
+        [
+            this.allLogsPath,
+            this.mediumLogsPath,
+            this.highLogsPath
+        ].forEach(path => {
+            if (!fs.existsSync(path)) {
+                fs.writeFileSync(path, '');
+            }
+        });
 
     }
 
-    save(log: LogEntity): Promise<void> {
-        throw new Error("Method not implemented.");
+    async save(newLog: LogEntity): Promise<void> {
+        const logAsJson ='${JSON.stringify(newLog)}\n'
+
+
+        fs.appendFileSync(this.allLogsPath, logAsJson)
+
+        if(newLog.level === LogSeverityLevel.low) return;
+
+        if(newLog.level === LogSeverityLevel.medium){
+
+        fs.appendFileSync(this.mediumLogsPath,logAsJson)
+        
+        }else{
+            
+            fs.appendFileSync(this.highLogsPath, logAsJson)
+
+        }
+
+
+
+
     }
 
 
