4e27d159b6ea632867c8363e57992db8b9caa758
Jorge <jorgereyeslira02@gmail.com>
2024-09-17 23:13:37 -0500

    getLogs

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -18,4 +18,15 @@ export class LogEntity{
         this.level = level
         this.createdAt = new Date()
     }
+
+
+    static fromJson = (json:string):LogEntity   => {
+        const {message,level,createdAt} = JSON.parse(json)
+
+
+        const log = new LogEntity(message,level)
+        log.createdAt = new Date(createdAt)
+        return log
+    
+    }
 }
--- src/infrastrucutre/datasources/file-system.datasource.ts
+++ src/infrastrucutre/datasources/file-system.datasource.ts
@@ -59,10 +59,36 @@ export class FileSystemDatasource implements LogDataSource {
 
     }
 
+    private getLogsFromFile = (path: string):LogEntity[] => {
+        const content = fs.readFileSync(path, 'utf-8')
+        
+        const logs = content.split('\n').map(
+            log => LogEntity.fromJson(log)
+        )
+
+        return logs
+    }
 
 
-    getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
-        throw new Error("Method not implemented.");
+    async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+        
+        switch (severityLevel) {
+            case LogSeverityLevel.low:
+                return this.getLogsFromFile(this.allLogsPath)
+
+            case LogSeverityLevel.medium:
+                return this.getLogsFromFile(this.mediumLogsPath)
+
+
+            case LogSeverityLevel.high:
+                return this.getLogsFromFile(this.highLogsPath)
+
+        
+            default:
+            
+            throw new Error(`#{severityLevel} Method not implemented.}`);
+        }
+        
     }
 
 }
