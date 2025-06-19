6d07855c0a3082262cafdca3f939edd74024a15f
Jorge <jorgereyeslira02@gmail.com>
2025-06-19 17:18:17 -0500

    Vizualizando como trabaja CRON

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,19 @@
+import {CronJob} from 'cron'
+
 export class Server{
 
  public static start(){
         console.log('Server Running...')
+
+        const job = new CronJob(
+	'*/10  * * * * *', // cronTime  second, minute, hour, day of month, month
+	function () {
+		console.log('You will see this message every 2 second');
+	}, // onTick
+	null, // onComplete
+	true, // start
+	'America/Los_Angeles' // timeZone
+);
+// job.start() is optional here because of the fourth parameter set to tru
     }
 }
