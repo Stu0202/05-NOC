05ef7c57bbb3cc3f7a9633c92896cc47a906d3f9
Jorge <jorgereyeslira02@gmail.com>
2025-06-19 23:35:00 -0500

    Creando el FileSystem-SaveLog para guardar los Logs de acuerdo a su nivel de severidad

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -8,12 +8,12 @@ import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
 export class FileSystemDataSource implements LogDatasource{
 
     private readonly logPath = 'logs/';
-    private readonly allLogsPath = 'logs/logs-low.log';
+    private readonly allLogsPath = 'logs/logs-all.log';
     private readonly mediumLogsPath = 'logs/logs-medium.log';
     private readonly highLogsPath = 'logs/logs-high.log';
 
     constructor(){
-
+        this.createLogsFiles();
     }
 
     private createLogsFiles = () => {
@@ -21,11 +21,37 @@ export class FileSystemDataSource implements LogDatasource{
         if(!fs.existsSync(this.logPath)){
             fs.mkdirSync(this.logPath)
         }
+
+        [
+            this.allLogsPath,
+            this.mediumLogsPath,
+            this.highLogsPath
+
+        ].forEach( path => {
+
+            if(fs.existsSync(path))return;
+            
+            fs.writeFileSync(path, '')
+
+        })
     }
 
 
-    saveLogs(log: LogEntity): Promise<void> {
-        throw new Error("Method not implemented.");
+  async  saveLogs(newLog: LogEntity): Promise<void> {
+       
+        const logAsJson = `${JSON.stringify(newLog)}\n`
+
+        fs.appendFileSync(this.allLogsPath, logAsJson) // appendFile... va al archivo y crea una linea al final
+
+        if(newLog.level === LogSeverityLevel.low) return
+
+        if(newLog.level === LogSeverityLevel.medium){
+            fs.appendFileSync(this.mediumLogsPath, logAsJson)
+        }else{
+            
+            fs.appendFileSync(this.highLogsPath, logAsJson)
+        }
+
     }
     getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
         throw new Error("Method not implemented.");
