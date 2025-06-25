ae99fa4b06097551b211b5e9da2af8615c177023
Jorge <jorgereyeslira02@gmail.com>
2025-06-25 17:03:32 -0500

    Probando la implementacion

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -32,8 +32,8 @@ export class CheckService implements CheckServiceUseCase{
             return true
         } catch (error) {
 
-            const errorMessage =  `${error}`
-            const log = new LogEntity(errorMessage, LogSeverityLevel.low)
+            const errorMessage =  `${url} is not ok  ${error}`
+            const log = new LogEntity(errorMessage, LogSeverityLevel.high)
             this.logRepository.saveLogs(log)
 
             this.errorCallBack(errorMessage)
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,13 @@
 import { error } from "console";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { CronService } from "./cron/cron-service"
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
+import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 
+
+const fileSystemlogRepository = new LogRepositoryImpl(
+    new FileSystemDataSource()
+)
 
 export class Server{
 
@@ -11,8 +17,9 @@ export class Server{
             '*/5 * * * * *',
             () => {
                // new CheckService().execute('https://google.com')
-               const url = 'https://google.com'
+               const url = 'http://localhost:3000'
                 new CheckService(
+                    fileSystemlogRepository,
                     () => console.log(`${url} is ok` ),
                     (error) => console.log(error)
                 ).execute(url)
