0657e2d947a63d721e163432eb76a5e0e59587eb
Jorge <jorgereyeslira02@gmail.com>
2025-08-13 17:18:29 -0500

    Moongose

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import 'dotenv/config'
+import {envs} from './config/plugin/envs.plugin'
+import { MongoDataBase } from './data/mongo';
 import { Server } from "./presentation/server";
-import { envs } from './config/plugin/envs.plugin';
 
 
 (async()=>{
     main()
 })();
 
-function main(){
+async function main(){
 
-    Server.start()
-   // console.log(envs)
+    await MongoDataBase.connect({mongoUrl: envs.MONGO_URL, dbName: envs.MONGO_DB_NAME});
+
+    //Server.start()
+  
 }
 
