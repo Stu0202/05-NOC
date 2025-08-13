359eb74521907e171278430a11e9210a7c4c344c
Jorge <jorgereyeslira02@gmail.com>
2025-08-13 17:28:03 -0500

    Esquema y modelos Mongo

---
+++ src/data/mongo/models/log.model.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const logSchema = new mongoose.Schema({
+    message: {
+        type: String,
+        require: true,
+
+    },
+    origin: {
+        type: String,
+    },
+    level: {
+        type: String,
+        enum: ['low', 'medium','high'],
+        default: 'low'
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+})
